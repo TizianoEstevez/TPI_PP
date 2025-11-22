d797178442d5c3d4d8d9439c0446b02b04433875
TizianoEstevez <tiziestevez78@gmail.com>
2025-11-22 15:19:59 -0300

    Remove extra blank line in Mensajes.java

--- Enum/Mensajes.java
+++ Enum/Mensajes.java
@@ -5,5 +5,4 @@ public class Mensajes {
     public static final String ABONO_VIGENTE = "El usuario tiene un abono mensual vigente.";
     public static final String ABONO_NO_VIGENTE = "El usuario no tiene un abono mensual vigente.";
     public static final String NO_EXISTEN_ABONOS = "No hay abonos registrados para esta cuenta.";
-    
 }
